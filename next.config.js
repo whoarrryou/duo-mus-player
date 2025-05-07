22f72d37df57e774d2ec5790de3257f451468491
whoareyou <krbavadiya11@gmail.com>
2025-05-07 22:34:05 +0530

    again qttt

--- next.config.js
+++ next.config.js
@@ -7,6 +7,21 @@ const nextConfig = {
     });
     return config;
   },
+  experimental: {
+    serverComponentsExternalPackages: ['socket.io'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/socket',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig; 
